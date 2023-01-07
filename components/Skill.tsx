46642f14b3a23fb17add96ec20beac79878231cb
RamkishoreBandla <bandlaramkishore090@gmail.com>
2023-01-06 21:07:31 -0500

    updated skills

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { motion } from 'framer-motion';
 type Props = {
-    directionLeft?: boolean
+    directionLeft?: boolean,
+  skillDetails:{
+    imageUrl:string,
+    percentage:String,
+    name:String
+  }
 }
 
-export default function  Skill({ directionLeft }: Props) {
+export default function  Skill({ directionLeft,skillDetails }: Props) {
     return (
         <div className='group realtive flex cursor-pointer'>
             <motion.img
@@ -14,7 +19,7 @@ export default function  Skill({ directionLeft }: Props) {
                 }}
                 transition={{ duration: 1 }}
                 whileInView={{ opacity: 1, x: 0 }}
-                src="https://cdn-icons-png.flaticon.com/512/5968/5968292.png"
+                src={skillDetails.imageUrl}
                 className='rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28
         xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
             />
@@ -22,7 +27,7 @@ export default function  Skill({ directionLeft }: Props) {
             ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0'>
                 <div className='flex items-center justify-center h-full'>
                     <p className='text-3xl font-bold text-black opacity-100'>
-                        100%
+                       {skillDetails.percentage}
                     </p>
                 </div>
             </div>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -4,6 +4,67 @@ import Skill from './Skill';
 type Props = {}
 
 export default function Skills({}: Props) {
+
+  const skillDetails=[
+    {
+      imageUrl:"https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+    percentage:"90%",
+    name:"Javascript"
+    },
+    {
+      imageUrl:"https://cdn-icons-png.flaticon.com/512/5968/5968381.png",
+    percentage:"80%",
+    name:"TypeScript"
+    },
+    {
+      imageUrl:"https://cdn-icons-png.flaticon.com/512/875/875209.png",
+    percentage:"90%",
+    name:"React.js"
+    },
+    {
+      imageUrl:"https://as2.ftcdn.net/v2/jpg/02/95/03/85/1000_F_295038583_mn0uxJ6A0YO57HA4xXQqHFUjiW1BcqBE.jpg",
+    percentage:"80%",
+    name:"Angular"
+    },
+    {
+     
+      imageUrl:"https://cdn-icons-png.flaticon.com/512/919/919825.png",
+      percentage:"80%",
+      name:"Node.js"
+    },
+    {
+     
+      imageUrl:"https://cdn-icons-png.flaticon.com/512/919/919836.png",
+      percentage:"80%",
+      name:"MySQL"
+    },
+    {
+      imageUrl:"./mongodb.jpg",
+      percentage:"80%",
+      name:"MongoDB"
+    },
+    {
+      imageUrl:"https://cdn-icons-png.flaticon.com/512/5968/5968282.png",
+      percentage:"80%",
+      name:"Java"
+    },
+    {
+      imageUrl:"https://cdn-icons-png.flaticon.com/512/5968/5968350.png",
+      percentage:"80%",
+      name:"Python"
+    },
+    {
+      imageUrl:"https://cdn-icons-png.flaticon.com/512/5969/5969059.png",
+      percentage:"80%",
+      name:"Docker"
+    },
+    {
+      imageUrl:"https://cdn-icons-png.flaticon.com/512/2085/2085263.png",
+      percentage:"80%",
+      name:"HTML , CSS"
+    }
+]
+
   return (
     <motion.div 
     initial={{ opacity: 0 }}
@@ -21,13 +82,7 @@ export default function Skills({}: Props) {
         </h3>
 
             <div className='grid grid-cols-4 gap-5'>
-                <Skill/>
-                <Skill/>
-                <Skill/>
-                <Skill/>
-                <Skill/>
-                <Skill/>
-                <Skill/>
+              {skillDetails.map((skill,i)=> <Skill key={i} skillDetails={skill}/>)}
             </div>
             
     </motion.div>
